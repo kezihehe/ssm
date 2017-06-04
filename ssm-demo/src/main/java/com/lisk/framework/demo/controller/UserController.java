9deaf3f8ffd5e745737afd30d19a0030bcd8bbd1
lishengke <shengke.li@ds365.com>
2017-06-04 17:02:05 +0800

    编辑 增加swagger注解

--- ssm-demo/src/main/java/com/lisk/framework/demo/controller/UserController.java
+++ ssm-demo/src/main/java/com/lisk/framework/demo/controller/UserController.java
@@ -2,9 +2,12 @@ package com.lisk.framework.demo.controller;
 
 import com.lisk.framework.demo.domain.User;
 import com.lisk.framework.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,23 +18,25 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/user")
+@Api(value = "用户操作")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "获取用户列表",notes = "查询所有用户列表，不分页")
     public List<User> list(){
         return userService.findAll();
     }
 
-    @RequestMapping("/addui")
+    @RequestMapping(value = "/addui",method = RequestMethod.GET)
     public String addui(){
         return "add";
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String hello(HttpServletRequest request){
         return "Hello";
